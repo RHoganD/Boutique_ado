3082f2463eca4c8dac15e3e4b91642865e26c241
RHoganD <richpatric@hotmail.com>
2023-08-24 04:20:09 +0000

    Add bag content and delivery logic

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents', # this is from context.py. here will make it avaible to all template in the project
             ],
         },
     },
@@ -154,6 +155,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
